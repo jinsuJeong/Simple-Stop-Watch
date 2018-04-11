b00e907cc9bc15f7c4a27b1bb8d335b0f974952c
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-04-11 13:32:52 +0900

    stopwatch

--- Simple Stop Watch/ViewController.swift
+++ Simple Stop Watch/ViewController.swift
@@ -17,17 +17,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()})
+        //myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()})
     }
 
     func updateTime(){
         count+=1
         let min = count / 60 / 100
-        let sec = (count - (min * 60 - 100)) / 100
+        let sec = (count - (min * 60 * 100)) / 100
         let msec = count - (min * 60 * 100) - (sec * 100)
         timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
         
     }
-
+    @IBAction func timeStart(_ sender: Any) {
+         myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()})
+    }
+    @IBAction func timeStop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func timeReset(_ sender: Any) {
+        count=0
+        timeLabel.text="00:00:00"
+    }
 }
 
